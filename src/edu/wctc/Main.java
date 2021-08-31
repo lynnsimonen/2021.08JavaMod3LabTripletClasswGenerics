877c49fcffb4416b2af69428fc14d62273fa7324
Lynn Simonen <lsimonen@my.wctc.edu>
2021-08-31 08:06:23 -0500

    Initial commit

---
+++ src/edu/wctc/Main.java
@@ -0,0 +1,11 @@
+package edu.wctc;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Triplet snoopy = new Triplet(peanuts, white, medium);
+        Triplet santasLittleHelper = new Triplet(simpsons, brown, small);
+        Triplet clifford = new Triplet(clifford, red, big)
+    }
+}
