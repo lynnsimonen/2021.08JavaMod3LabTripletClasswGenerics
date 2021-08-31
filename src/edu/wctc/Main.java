92b7b41e54d393cf66dab8d3d3bd010ab765c2a0
lynnsimonen <79551770+lynnsimonen@users.noreply.github.com>
2021-08-31 12:21:28 -0500

    General Class object creation update
    following instructions for creating general class objects

--- src/edu/wctc/Main.java
+++ src/edu/wctc/Main.java
@@ -3,9 +3,19 @@ package edu.wctc;
 public class Main {
 
     public static void main(String[] args) {
-
-        Triplet snoopy = new Triplet(peanuts, white, medium);
-        Triplet santasLittleHelper = new Triplet(simpsons, brown, small);
-        Triplet clifford = new Triplet(cliffordTheBigRedDog, red, big);
+      
+        Triplet<String> snoopy = new Triplet<>();
+            snoopy.first("Peanuts");
+        
+        Triplet<String> santasLittleHelper = new Triplet<>();
+            santasLittleHelper.second("The Simpsons");
+        
+        Triplet<String> clifford = new Triplet<>();
+            clifford.third("Clifford, The Big Red Dog");
+        
+//         Triplet snoopy = new Triplet(peanuts, white, medium);
+//         Triplet santasLittleHelper = new Triplet(simpsons, brown, small);
+//         Triplet clifford = new Triplet(cliffordTheBigRedDog, red, big);
+        
     }
 }
