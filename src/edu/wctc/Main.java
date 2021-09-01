42504b604b68a421a39c1017a8518cee8f11b71f
lynnsimonen <79551770+lynnsimonen@users.noreply.github.com>
2021-09-01 18:44:40 -0500

    Updated changes and now the program runs correctly

--- src/edu/wctc/Main.java
+++ src/edu/wctc/Main.java
@@ -3,19 +3,25 @@ package edu.wctc;
 public class Main {
 
     public static void main(String[] args) {
-      
-        Triplet<String> snoopy = new Triplet<>();
-            snoopy.first("Peanuts");
-        
-        Triplet<String> santasLittleHelper = new Triplet<>();
-            santasLittleHelper.second("The Simpsons");
-        
-        Triplet<String> clifford = new Triplet<>();
-            clifford.third("Clifford, The Big Red Dog");
-        
-//         Triplet snoopy = new Triplet(peanuts, white, medium);
-//         Triplet santasLittleHelper = new Triplet(simpsons, brown, small);
-//         Triplet clifford = new Triplet(cliffordTheBigRedDog, red, big);
-        
+
+        Triplet<String> siblings = new Triplet<>("Ethan", "Lucas", "Wylie");
+        Triplet<Integer> favNumbers = new Triplet<>(3, 24, 68);
+        Triplet<Character> favLetters = new Triplet<>('a', 'b', 'c');
+
+
+        System.out.println(siblings);
+        System.out.println(favLetters);
+        System.out.println(favNumbers);
+
+
+        //    Triplet<String> siblings = new Triplet<>();
+    //        siblings.add(new Triplet("Ethan", "Lucas", "Wylie"));
+    //        siblings.add(new Triplet("Ethan", "Lucas", "Wylie"));
+    //                Triplet<int> favNumbers = new Triplet<>();
+    //                favNumbers.setSecond(3, 24, 68);
+
+        //for (Triplet triplet : .values()) {   <--This is for loops
+        //   System.out.println(triplet);        }
+
     }
 }
--- src/edu/wctc/Triplet.java
+++ src/edu/wctc/Triplet.java
@@ -1,9 +1,8 @@
 package edu.wctc;
 
-public class Triplet {
-    private T first;
-    private T second;
-    private T third;
+public class Triplet<T> {
+    private T first, second, third;
+
 
     public Triplet(T first, T second, T third) {
         this.first = first;
@@ -33,5 +32,13 @@ public class Triplet {
 
     public void setThird(T third) {
         this.third = third;
+    }
+
+    @Override
+    public String toString() {
+        //Can use a string to print out any data type or for String.format, you can use
+        //if else statement if(first instance of Boolean)... else (first instance of String)...
+        //return %d or %s ...
+        return String.format("First: %s, Second: %s, Third: %s",first, second, third);
     }
 }
